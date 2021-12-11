f9faf5bd509fc338e9096b248337ed51ed8f75c7
iSebastianCabrera <jscrojas25@yahoo.es>
2021-12-10 22:50:33 -0500

    generando mas test

--- src/main/java/com/example/demo/services/Servicio.java
+++ src/main/java/com/example/demo/services/Servicio.java
@@ -12,10 +12,23 @@ public class Servicio {
     public Mono<String> buscarUno() {
         return Mono.just("Pedro");
     }
+
+
     public Flux<String> buscarTodos() {
         return Flux.just("Pedro", "Maria", "Jesus", "Carmen");
     }
+
+
     public Flux<String> buscarTodosLento() {
         return Flux.just("Pedro", "Maria", "Jesus", "Carmen").delaySequence(Duration.ofSeconds(20));
     }
+    public Flux<String> buscarTodosFiltro() {
+        Flux<String> source = Flux.just("John", "Monica", "Mark", "Cloe", "Frank", "Casper", "Olivia", "Emily", "Cate")
+                .filter(name -> name.length() == 4)
+                .map(String::toUpperCase)
+                .concatWith(Mono.error(new IllegalArgumentException("Mensaje de Error"))
+        );
+        return source;
+    }
+
 }
